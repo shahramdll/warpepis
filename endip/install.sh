1fcf9d037a1dfda5174941aa9dd3a5ac40119747
Shahram <shahram.dll@gmail.com>
2026-08-14 00:28:12 +0330

    Update install.sh

--- endip/install.sh
+++ endip/install.sh
@@ -43,26 +43,10 @@ endipv4() {
 	n=0
 	iplist=100
 	while true; do
-		temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
-		n=$(($n + 1))
-		if [ $n -ge $iplist ]; then
-			break
-		fi
 		temp[$n]=$(echo 8.6.112.$(($RANDOM % 256)))
 		n=$(($n + 1))
 		if [ $n -ge $iplist ]; then
 			break
-		fi
-		temp[$n]=$(echo 162.159.193.$(($RANDOM % 256)))
-		n=$(($n + 1))
-		if [ $n -ge $iplist ]; then
-			break
-		fi
-		temp[$n]=$(echo 162.159.195.$(($RANDOM % 256)))
-		n=$(($n + 1))
-		if [ $n -ge $iplist ]; then
-			break
-		fi
 		temp[$n]=$(echo 188.114.96.$(($RANDOM % 256)))
 		n=$(($n + 1))
 		if [ $n -ge $iplist ]; then
